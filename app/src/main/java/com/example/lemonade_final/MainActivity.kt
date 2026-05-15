e0d0b044a4b4ec89895f73b6bc6c7e398bdd57c9
tmoraisflc <tmoraisfl@gmail.com>
2026-05-15 12:42:09 -0300

    refactor: update package name and improve Lemonade game logic

--- app/src/main/java/com/example/lemonade_final/MainActivity.kt
+++ app/src/main/java/com/example/lemonade_final/MainActivity.kt
@@ -1,19 +1,14 @@
-package com.example.lemonade_final
+package com.example.lemonadeapp
 
-import android.os.Bundle
-import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material3.Button
-import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
@@ -23,58 +18,38 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import com.example.lemonade_final.ui.theme.Lemonade_finalTheme
+import com.example.lemonade_final.R
 
-class MainActivity : ComponentActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContent {
-            Lemonade_finalTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    LemonadeScreen(
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
-            }
-        }
-    }
-}
 
-enum class LemonadeStep() {
-    TREE, LEMON, LEMONADE, EMPTY_GLASS  // status da tela
-
-}
+enum class LemonadeStepByStep { TREE, LEMON, LEMONADE, EMPTY_GLASS }
 
 @Composable
 fun LemonadeScreen(modifier: Modifier = Modifier) {
-    var lemonadeStep by remember { mutableStateOf(LemonadeStep.TREE) }
+    var lemonadeSteps by remember { mutableStateOf(LemonadeStepByStep.TREE) }
+    var lemonadeImage: Int
+    var lemonadeContentDescription: Int
+    var lemonadeDescription: Int
+    var count: Int = (2..4).random()
+    var click = 1
 
-
-    var lemonadeImage = R.drawable.lemon_tree   // nao precisa do primeiro?
-    var lemonadeContentDescription = R.string.lemon_tree
-    var lemonadeDescription = R.string.tap_lemon_tree
-
-
-    when (lemonadeStep) {
-        LemonadeStep.TREE -> {
+    when (lemonadeSteps) {
+        LemonadeStepByStep.TREE -> {
             lemonadeImage = R.drawable.lemon_tree
             lemonadeContentDescription = R.string.lemon_tree
             lemonadeDescription = R.string.tap_lemon_tree
         }
 
-        LemonadeStep.LEMON -> {
+        LemonadeStepByStep.LEMON -> {
             lemonadeImage = R.drawable.lemon_squeeze
             lemonadeContentDescription = R.string.lemon
             lemonadeDescription = R.string.tap_squeeze
         }
 
-        LemonadeStep.LEMONADE -> {
+        LemonadeStepByStep.LEMONADE -> {
             lemonadeImage = R.drawable.lemon_drink
             lemonadeContentDescription = R.string.glass_of_lemonade
             lemonadeDescription = R.string.tap_lemonade_drink
@@ -88,101 +63,76 @@ fun LemonadeScreen(modifier: Modifier = Modifier) {
     }
 
     Column(
-        modifier = modifier
-    ) {
-
-        // 1. Estado para controlar em qual passo estamos (1, 2, 3 ou 4)
-        var currentStep by remember { mutableStateOf(1) }
-
-// 2. Estado para armazenar quantos cliques faltam
-        var squeezeCount by remember { mutableStateOf(0) }
-
-
-
-        Button(
+        modifier.clickable(
             onClick = {
-                if (lemonadeStep == LemonadeStep.TREE) {
-                    // 1. Antes de ir para o limão, sorteia o número
-                    squeezeCount = (2..4).random()
-                    lemonadeStep = LemonadeStep.LEMON
-                } else if (lemonadeStep == LemonadeStep.LEMON) {
-                    // 2. No limão, diminui 1 ponto a cada clique
-                    squeezeCount--
-
-                    // 3. SÓ MUDA se a conta chegar a zero
-                    if (squeezeCount == 0) {
-                        lemonadeStep = LemonadeStep.LEMONADE
+                lemonadeSteps = when (lemonadeSteps) {
+                    LemonadeStepByStep.TREE -> LemonadeStepByStep.LEMON
+                    LemonadeStepByStep.LEMON -> {
+                        if (count == click) {
+                            count = (2..4).random()
+                            LemonadeStepByStep.LEMONADE
+                        }
+                        else {
+                            click++
+                            LemonadeStepByStep.LEMON
+                        }
                     }
-                } else if (lemonadeStep == LemonadeStep.LEMONADE) {
-                    lemonadeStep = LemonadeStep.EMPTY_GLASS
-                } else {
-                    lemonadeStep = LemonadeStep.TREE
+                    LemonadeStepByStep.LEMONADE -> LemonadeStepByStep.EMPTY_GLASS
+                    else -> LemonadeStepByStep.TREE
                 }
             }
-        ) { LemonTree(lemonadeImage, lemonadeContentDescription, lemonadeDescription) }
+        )
+    ) {
+        LemonadeItem(
+            image = lemonadeImage,
+            contentDescription = lemonadeContentDescription,
+            description = lemonadeDescription,
+            modifier
+        )
     }
 }
 
-@Preview
 @Composable
-private fun LemonadeScreenPreview(
-
+fun LemonadeItem(
+    image: Int,
+    contentDescription: Int,
+    description: Int,
+    modifier: Modifier
 ) {
-    
-}
-
-
-@Composable
-fun LemonTree(selectedImage: Int, contentDescriptionImage: Int, description: Int) {
     Column(
         horizontalAlignment = Alignment.CenterHorizontally,
         verticalArrangement = Arrangement.Center,
         modifier = Modifier
             .fillMaxSize()
-            //.padding(all = 12.dp)     //esse padding nao ta mudando nada?
+            .padding(all = 12.dp)
     ) {
         Image(
-            painterResource(selectedImage),
-            contentDescription = stringResource(contentDescriptionImage),
-            modifier = Modifier
-                .background(color = Color.Green, shape = RoundedCornerShape(24.dp))
+            painterResource(image),
+            contentDescription = stringResource(contentDescription),
+            modifier
+                .background(color = Color.Gray, shape = RoundedCornerShape(24.dp))
                 .padding(horizontal = 24.dp)
                 .size(220.dp)
+
         )
         Text(
             stringResource(description),
-            modifier = Modifier.padding(top = 24.dp)
+            modifier.padding(top = 24.dp)
         )
     }
 }
 
-
 @Preview(showBackground = true)
 @Composable
-private fun LemonTreePreview() {
-    Lemonade_finalTheme {
-        LemonTree(
-            R.drawable.lemon_drink,
-            contentDescriptionImage = R.string.glass_of_lemonade,
-            description = R.string.tap_lemonade_drink
-        )
+fun LemonadeItemPreview() {
+    LemonadeAppTheme {
+        Column() {
+            LemonadeItem(
+                R.drawable.lemon_tree,
+                R.string.lemon_tree_content_description,
+                R.string.tap_lemon_tree,
+                modifier = Modifier
+            )
+        }
     }
-
-}
-
-@Composable
-fun Lemonade(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    Lemonade_finalTheme {
-        Lemonade("Android")
-    }
-}
+}..
